eed3a4c67cf321d8693da945cd027ad2c9a86959
Ben Miller <bmiller@ctacorp.com>
2011-04-06 14:45:40 +0000

    Created blank project (1.9.2 / rails 3.0.4)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommentToolApp::Application.config.secret_token = '04b43dc045ad813768144d4aa6adfe1e36b11f147d05b043f917855a54b8cd66b2421f50b38c7c842dd48e5546ce86d6c2510b7af7c6d1b2fa45718b08a73802'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CommentToolApp::Application.config.session_store :cookie_store, :key => '_comment_tool_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CommentToolApp::Application.config.session_store :active_record_store
