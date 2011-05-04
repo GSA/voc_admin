c14256fe79d5e4e3e8ac3f41d519642e61c8d73b
Ben Miller <bmiller@ctacorp.com>
2011-05-04 18:51:10 +0000

    changed triggers to checkboxes, cleaned up controller, fixed rule trigger validation, added triggers to rule show page

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -11,27 +11,27 @@ class RulesController < ApplicationController
   
   def new
     @rule = @survey_version.rules.build
-    @source_array = @survey_version.questions.collect {|q| ["#{q.assetable.question_content.id},QuestionContent", q.assetable.question_content.statement + "(question)"]}
-    @source_array.concat(@survey_version.display_fields.collect {|df| ["#{df.id},#{df.type}", df.name + "(display field)"]})
+    build_source_array
+    do_rule_builds
   end
   
   def create
-    @source_array = @survey_version.questions.collect {|q| ["#{q.assetable.question_content.id},QuestionContent", q.assetable.question_content.statement + "(question)"]}
-    @source_array.concat(@survey_version.display_fields.collect {|df| ["#{df.id},#{df.type}", df.name + "(display field)"]})
+    build_source_array
     @rule = @survey_version.rules.build params[:rule]
     @rule.rule_order = @survey_version.rules.count + 1
     
     if @rule.save
       redirect_to [@survey, @survey_version,@rule]
     else
+      do_rule_builds
       render :new
     end
   end
   
   def edit
     @rule = @survey_version.rules.find(params[:id])
-    @source_array = @survey_version.questions.collect {|q| ["#{q.assetable.question_content.id},QuestionContent", q.assetable.question_content.statement + "(question)"]}
-    @source_array.concat(@survey_version.display_fields.collect {|df| ["#{df.id},#{df.type}", df.name + "(display field)"]})
+    build_source_array
+    do_rule_builds
   end
   
   def update
@@ -40,6 +40,8 @@ class RulesController < ApplicationController
     if @rule.update_attributes(params[:rule])
       redirect_to survey_survey_version_rule_path(@survey, @survey_version, @rule)
     else
+      build_source_array
+      do_rule_builds
       render :edit
     end
   end
@@ -56,5 +58,16 @@ class RulesController < ApplicationController
   def get_survey_version
     @survey = Survey.find(params[:survey_id])
     @survey_version = @survey.survey_versions.find(params[:survey_version_id])
+  end
+  
+  def do_rule_builds
+    @rule.execution_trigger_rules.build if  @rule.execution_trigger_rules.size == 0
+    @rule.actions.build if @rule.actions.size == 0
+    @rule.criteria.build if @rule.criteria.size == 0
+  end
+  
+  def build_source_array
+    @source_array = @survey_version.questions.collect {|q| ["#{q.assetable.question_content.id},QuestionContent", q.assetable.question_content.statement + "(question)"]}
+    @source_array.concat(@survey_version.display_fields.collect {|df| ["#{df.id},#{df.type}", df.name + "(display field)"]})   
   end
 end
