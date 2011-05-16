4b52baf5715ae5baf8e7c207a294ae3c6d17d7e4
Ben Miller <bmiller@ctacorp.com>
2011-05-16 04:28:30 +0000

    adding initializer for oracle

---
+++ config/initializers/oracle_config.rb
@@ -0,0 +1,10 @@
+if ActiveRecord::Base.connection.adapter_name.downcase.match /oracle/
+  ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter.class_eval do
+    
+    # http://wiki.github.com/rsim/oracle-enhanced/usage
+    
+    self.default_sequence_start_value = "1 NOCACHE INCREMENT BY 1"
+    self.emulate_integers_by_column_name = true
+    self.emulate_booleans_from_strings = true
+  end
+end
