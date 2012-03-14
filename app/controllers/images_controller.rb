da4cebf8d48b3341136d638e9ee1ae1291303254
Juan Alvarado <jalvarado@ctacorp.com>
2012-03-14 21:09:41 +0000

    change upload path for production environment

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,16 +1,16 @@
 class ImagesController < ApplicationController
-  Images_Folder = "hhs_image_lib/"
+  Images_Folder = Rails.env.production? ? "/appdata/uploads/" : "#{Rails.root}/public/images/hhs_image_lib/"
   
   #show list of files
   def index
-    @files = Dir["#{Rails.root}/public/images/#{Images_Folder}*"]
+    @files = Dir["#{Images_Folder}*"]
     @files.map! {|f| f.split('/').last}
     @files.sort!
   end
   
   def display
     return(redirect_to images_path) if params['file'].nil?
-    @file = Images_Folder.to_s + params['file']
+    @file = params['file']
   end
   
   def new
@@ -19,7 +19,7 @@ class ImagesController < ApplicationController
   def save_file
     return(redirect_to new_image_path) if params['upload']['datafile'].blank?
     name = params['upload']['datafile'].original_filename
-    directory = "public/images/"+Images_Folder
+    directory = Images_Folder
     path = directory+name
     File.open(path, "wb") {|f| f.write(params['upload']['datafile'].read)}
     flash[:notice] = 'Image was successfully uploaded.'
@@ -29,7 +29,7 @@ class ImagesController < ApplicationController
   #remove a file
   def remove
     return(redirect_to images_path) if params['file'].nil?
-    File.delete("public/images/"+Images_Folder+params['file'])
+    File.delete(Images_Folder+params['file'])
     flash[:notice] = 'Image was successfully deleted.'
     redirect_to images_path
   end
