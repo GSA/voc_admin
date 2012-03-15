927ced94e9938401b8e580c5705872a74d286634
Ben Miller <bmiller@ctacorp.com>
2012-03-15 01:36:43 +0000

    fixed issue with permissions

--- sync_uploads.sh
+++ sync_uploads.sh
@@ -1,1 +1,1 @@
-rsync -rcv --chmod=ug=rwX -e "ssh -i /appdata/id_rsa" ./../uploads/ hhsweb@comment-ws-01:/var/www/voc/uploads/
+rsync -rcv --chmod=uog=rX -e "ssh -i /appdata/id_rsa" ./../uploads/ hhsweb@comment-ws-01:/var/www/voc/uploads/
