0ee3cddd55a751b47752d594d0149aa3741a8501
Juan Alvarado <jalvarado@ctacorp.com>
2012-03-30 20:49:05 +0000

    add mailer login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,17 @@ CommentToolApp::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {:host => 'comment-adm.hhs.gov'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address                        => "smtp.mxes.net",
+    :port                           => 25,
+    :domain                         => 'ctacorp.com',
+    :user_name                      => 'emailalertbccs_paperworkreduction.gov', 
+    :password                       => 'icras555alerts',
+    :authentication                 => :login
+  }
 
   # Enable threaded mode
   # config.threadsafe!
