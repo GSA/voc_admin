e6323ae0e79e37f3afc934d447d3f901b0ae17bd
Heather Markman <hmarkman@ctacorp.com>
2012-04-02 15:13:53 +0000

    sortable arrows, starting to get them sorted.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,6 +19,6 @@ module ApplicationHelper
   def sortable column, title = nil
     title ||= column.titleize
     direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, :style => "display: inline-block; height: 100%; width: 100%;"
+    link_to title, {:sort => column, :direction => direction}
   end
 end
