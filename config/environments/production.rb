6b8ece3b90310452bf33db2daf1b9b35c5b2bed5
Juan Alvarado <jalvarado@ctacorp.com>
2012-04-03 12:01:16 +0000

    update default url for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ CommentToolApp::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {:host => 'comment-adm.hhs.gov'}
+  config.action_mailer.default_url_options = {:host => 'http://comment-adm.hhs.gov'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address                        => "smtp.mxes.net",
