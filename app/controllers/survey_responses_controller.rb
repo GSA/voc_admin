1c8552ae023434788b6abaa10511c05d32a43abc
Juan Alvarado <jalvarado@ctacorp.com>
2012-08-07 14:50:41 +0000

    checkbox display fields display one answer on each line

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -50,16 +50,16 @@ class SurveyResponsesController < ApplicationController
 
   ## You shouldn't be able to create survey responses from the admin app
   def create
-    # @client_id = SecureRandom.hex(64)
-    # @survey_version_id = params[:survey_version_id]
+    @client_id = SecureRandom.hex(64)
+    @survey_version_id = params[:survey_version_id]
     
-    # @survey_response = SurveyResponse.new ({:client_id => @client_id, :survey_version_id => @survey_version_id}.merge(params[:response]))
+    @survey_response = SurveyResponse.new ({:client_id => @client_id, :survey_version_id => @survey_version_id}.merge(params[:response]))
     
-    # @survey_response.raw_responses.each {|raw_response| raw_response.client_id = @client_id}
+    @survey_response.raw_responses.each {|raw_response| raw_response.client_id = @client_id}
     
-    # @survey_response.save!
+    @survey_response.save!
     
-    # redirect_to survey_responses_path
+    redirect_to survey_responses_path
   end
 
   def destroy
