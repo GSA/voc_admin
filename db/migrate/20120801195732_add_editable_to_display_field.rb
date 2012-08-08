af2730032df6bca5ecb1f7573df20143dd3c8df3
Juan Alvarado <jalvarado@ctacorp.com>
2012-08-08 13:25:41 +0000

    Update existing rules/display fields to the default values when adding the editable and action_type fields

--- db/migrate/20120801195732_add_editable_to_display_field.rb
+++ db/migrate/20120801195732_add_editable_to_display_field.rb
@@ -1,6 +1,9 @@
 class AddEditableToDisplayField < ActiveRecord::Migration
   def self.up
   	add_column :display_fields, :editable, :boolean, :default => 1
+
+    # Mark all existing display fields as non-editable
+    DisplayField.update_all ["display_fields.editable = ?", false]
   end
 
   def self.down
--- db/migrate/20120803132519_add_action_type_to_rules.rb
+++ db/migrate/20120803132519_add_action_type_to_rules.rb
@@ -1,6 +1,9 @@
 class AddActionTypeToRules < ActiveRecord::Migration
   def self.up
     add_column :rules, :action_type, :string, :default => 'db'
+
+    # Mark all existing rules as db action types
+    Rule.update_all ["action_type = ?", 'db']
   end
 
   def self.down
