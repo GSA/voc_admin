d2732ac9a887fd094e2065c1fb2b47e1d482c82c
Juan Alvarado <jalvarado@ctacorp.com>
2012-09-17 15:54:19 +0000

    add rule search

--- script/unmatched_df.rb
+++ script/unmatched_df.rb
@@ -1,4 +1,5 @@
 unmatched_display_fields = []
+unmatched_rules = []
 
 SurveyVersion.all.each do |sv|
   sv.display_fields.each do |df|
@@ -7,9 +8,19 @@ SurveyVersion.all.each do |sv|
        !sv.matrix_questions.any? { |mq| mq.choice_questions.any? { |cq| "#{mq.question_content.statement}: #{cq.question_content.statement}" == df.name} }
       unmatched_display_fields << df
     end
- end
+  end
+
+  sv.rules.each do |rule|
+    if !sv.choice_questions.any? {|cq| cq.question_content.statement == rule.name} &&
+       !sv.text_questions.any? {|tq| tq.question_content.statement == rule.name } &&
+       !sv.matrix_questions.any? {|mq| mq.choice_questions.any? {|cq| "#{mq.question_content.statement}: #{cq.question_content.statement}" == rule.name} }
+      unmatched_rules << rule
+    end
+  end
 end
 
 puts unmatched_display_fields.count
+puts unmatched_rules.count
 
 unmatched_display_fields.each {|df| df.update_attribute(:editable, true) }
+
