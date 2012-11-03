5039d53573205b1b637e794cd45f67d5dc915e48
Juan Alvarado <jalvarado@ctacorp.com>
2012-11-03 03:41:42 +0000

    move generation of options_for_select for answer_placement to choice question helper

--- app/helpers/choice_questions_helper.rb
+++ app/helpers/choice_questions_helper.rb
@@ -2,4 +2,8 @@ module ChoiceQuestionsHelper
   def options_for_answer_type(default)
     options_for_select([["Radio", ChoiceAnswer::RADIO], ["Dropdown", ChoiceAnswer::DROPDOWN], ["Multi-select", ChoiceAnswer::MULTISELECT], ["Check Boxes", ChoiceAnswer::CHECKBOX]], :selected => default)
   end
+
+  def options_for_answer_placement(default)
+    options_for_select([["horizontal (side-by-side)", ChoiceQuestion::HORIZONTAL_PLACEMENT], ["vertical (stacked)", ChoiceQuestion::VERTICAL_PLACEMENT]], :selected => default)
+  end
 end
