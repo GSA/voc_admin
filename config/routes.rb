5ea1acadd889f7232bb1a472dd1b03355836e5e1
Juan Alvarado <jalvarado@ctacorp.com>
2012-11-03 03:17:03 +0000

    specify the allowed routes for resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 CommentToolApp::Application.routes.draw do
-  match '/exports/:id/download' => "exports#download", :as => 'exports_download'
+  match '/exports/:id/download' => "exports#download",
+    :as => 'exports_download'
 
   get 'login' => 'user_sessions#new', :as => :login
   get 'logout' => 'user_sessions#destroy', :as => :logout
-  get 'reset_password' => 'user_sessions#reset_password', :as => :reset_password
-  resources :user_sessions do
+  get 'reset_password' => 'user_sessions#reset_password',
+    :as => :reset_password
+  resources :user_sessions, only: [:new, :create, :destroy] do
     post :do_pw_reset, :on => :collection
   end
 
@@ -13,8 +15,9 @@ CommentToolApp::Application.routes.draw do
 
   resources :surveys
 
-  resources :survey_responses do
-    get :export_all, :on => :collection, :as => 'export_all'
+  resources :survey_responses,
+    only: [:create, :index, :edit, :update, :destroy] do
+      get :export_all, :on => :collection, :as => 'export_all'
   end
 
   resources :rules do
@@ -22,34 +25,46 @@ CommentToolApp::Application.routes.draw do
     get :check_do_now, :on => :collection
   end
   resources :surveys do
-    resources :survey_versions do
+    resources :survey_versions,
+      only: [:show, :edit, :update, :destroy, :index] do
       get :publish, :on => :member, :as => "publish"
       get :unpublish, :on => :member, :as => "unpublish"
       get :clone_version, :on => :member, :as => "clone"
       get :edit_thank_you_page, :on => :member, :as => "edit_thank_you_page"
+
       resources :rules do
         put :increment_rule_order, :on => :member
         put :decrement_rule_order, :on => :member
         get :do_now, :on => :member
       end
-      resources :display_fields do
+
+      resources :display_fields,
+        only: [:new, :create, :edit, :update, :destroy, :index] do
         put :increment_display_order, :on => :member
         put :decrement_display_order, :on => :member
       end
-      resources :display_field_values
-      resources :custom_views
+
+      resources :display_field_values, only: [:edit, :update]
+      resources :custom_views,
+        only: [:new, :create, :edit, :update, :destroy, :index]
       resources :text_questions
-      resources :choice_questions
+      resources :choice_questions,
+        only: [:new, :create, :edit, :update, :destroy]
       resources :matrix_questions
-      resources :survey_elements do
+
+      # We don't need any default routes for survey_elements
+      resources :survey_elements, only: [] do
         post :up, :on => :member
         post :down, :on => :member
       end
-      resources :pages do
+
+      resources :pages, only: [:create, :update, :destroy] do
         post :move_page, :on => :member
         post :copy_page, :on => :member
       end
-      resources :assets
+
+      resources :assets,
+       only: [:new, :create, :edit, :update, :destroy]
       get :create_new_major_version, :on => :collection
       get :create_new_minor_version, :on => :member
     end
@@ -67,7 +82,9 @@ CommentToolApp::Application.routes.draw do
 
   # See how all your routes lay out with "rake routes"
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
+  # This is a legacy wild controller route that's not recommended for RESTful
+  # applications.
+  # Note: This route will make all actions in every controller accessible via
+  # GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
