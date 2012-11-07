705fac6909979d9b30da384d6d7748d97478cd84
Jake Vose <jvose@ctacorp.com>
2012-11-07 19:28:53 +0000

    additional tests for User model

--- spec/models/user_spec/user_spec.rb
+++ spec/models/user_spec/user_spec.rb
@@ -9,29 +9,49 @@ describe User do
     @valid_user.should be_valid
   end
   
-  it "is not valid without an email" do
-    @valid_user.email = nil
-    @valid_user.should_not be_valid
-  end
-  
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:f_name) }
+  it { should validate_presence_of(:l_name) }
+
   it "is not valid without a password" do
     @valid_user.password = nil
     @valid_user.password_confirmation = nil
     @valid_user.should_not be_valid
   end
   
-  it "is not valid without a first name" do
-    @valid_user.f_name = nil
-    @valid_user.should_not be_valid
-  end
-  
-  it "is not valid without a last name" do
-    @valid_user.l_name = nil
-    @valid_user.should_not be_valid
-  end
-
   it "should write the email to the database in lowercase" do
     email = "CAPITAL_EMAIL@TEST.COM"
     User.new(:email => email).email.should == email.downcase
+  end
+
+  it "should request default Survey scope if admin" do
+    User.any_instance.stub(:admin?).and_return(true)
+    Survey.stub(:scoped)
+    Survey.should_receive(:scoped)
+
+    @valid_user.surveys
+  end
+
+  it "should request User-and-Site-specific Surveys if not admin" do
+    User.any_instance.stub(:admin?).and_return(false)
+    Survey.stub(:includes).and_return(Survey)
+    Survey.stub(:where).and_return(Survey)
+
+    Survey.should_receive(:includes)
+    Survey.should_receive(:where)
+
+    @valid_user.surveys
+  end
+
+  it "should return true if user is admin" do
+    @valid_user.role = Role::ADMIN
+
+    @valid_user.admin?.should be_true
+  end
+
+  it "should return false if user is not admin" do
+    @valid_user.role = nil
+
+    @valid_user.admin?.should be_false
   end
 end
