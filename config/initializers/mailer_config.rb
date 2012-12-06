03836a3dd896c21dfbba143aa83ffcee458f1ffb
Juan Alvarado <jalvarado@ctacorp.com>
2012-12-06 15:06:45 +0000

    add some documentation to the mailer setup initializer

--- config/initializers/mailer_config.rb
+++ config/initializers/mailer_config.rb
@@ -1,3 +1,27 @@
+# mailer_config.rb
+# Read ActionMailer configuration options from mailer_settings.yml
+#
+# For more information about which settings may be set on
+# ActionMailer::Base go to:
+# http://api.rubyonrails.org/classes/ActionMailer/Base.html
+#
+#
+# Example mailer_settings.yml layout
+#
+# <Rails Environment>:
+#   raise_delivery_errors: false
+#   default_url_options: {:host => 'example.com'}
+#   delivery_method: <:smtp|:file|:send_mail>
+#   smtp_settings: { :address: <outgoing mail server>,
+#                    :port: <port>,
+#                    :domain: <sending domain>,
+#                    :user_name: <username>,
+#                    :password: <password>,
+#                    :authentication: <authentication type> }
+#   sendmail_settings: { :location: <location>,
+#                        :arguments: <command-line arguments> }
+#   file_settings: { :location: <directory to write emails> }
+
 begin
   MailConfig = YAML.load_file("#{Rails.root}/config/mailer_settings.yml")[Rails.env]
 
