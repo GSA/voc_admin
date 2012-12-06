54879fdaf6c1f8959b9665732032eff8eb894fa9
Juan Alvarado <jalvarado@ctacorp.com>
2012-12-06 14:57:56 +0000

    add a more detailed error message if configuation file is not found

--- config/initializers/mailer_config.rb
+++ config/initializers/mailer_config.rb
@@ -1,5 +1,9 @@
-MailConfig = YAML.load_file("#{Rails.root}/config/smtp_settings.yml")[Rails.env]
+begin
+  MailConfig = YAML.load_file("#{Rails.root}/config/mailer_settings.yml")[Rails.env]
 
-MailConfig.each_pair do |key, value|
-  ActionMailer::Base.send("#{key}=", value)
+  MailConfig.each_pair do |key, value|
+    ActionMailer::Base.send("#{key}=", value)
+  end
+rescue
+  raise "Missing configuration file for mail settings (config/mailer_settings.yml)"
 end
