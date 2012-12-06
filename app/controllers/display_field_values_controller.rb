680263098d0aa0dc000ae447f2b0fa6a052136ab
Jake Vose <jvose@ctacorp.com>
2012-12-06 20:14:29 +0000

    fix for Rule ExecutionTrigger::UPDATE in DisplayFieldValuesController.update

--- app/controllers/display_field_values_controller.rb
+++ app/controllers/display_field_values_controller.rb
@@ -16,7 +16,10 @@ class DisplayFieldValuesController < ApplicationController
 
     respond_to do |format|
       if @display_field_value.update_attributes(params[:display_field_value])
-        #@display_field_value.survey_response.process_me(2) # TODO: Reprocess response when a field is updated
+        
+        # Reprocess response when a field is updated
+        @display_field_value.survey_response.delay.process_me(2)
+        
         format.html {redirect_to root_url, :notice  => "Successfully updated display field."}
         format.js
       else
