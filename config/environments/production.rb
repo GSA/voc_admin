3012f2a9192f15b6a93b0e1c19df46d18651a9a7
Jake Vose <jvose@ctacorp.com>
2013-02-04 14:48:37 +0000

    removing rbconfig require statement since we don't care about host_os (this works on both platforms)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,6 @@ CommentToolApp::Application.configure do
   # mailer_settings.yml
 
   # Enable threaded mode (currently only in linux jruby and running in jruby_rack)
-  require 'rbconfig'
   config.threadsafe! if RUBY_PLATFORM == "java"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
