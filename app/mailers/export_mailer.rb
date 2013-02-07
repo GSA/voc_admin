f4e9f6a5ee9bac5c969f279acb6b7d344cfc3349
Jake Vose <jvose@ctacorp.com>
2013-02-07 20:48:26 +0000

    removing CTAC info from seeds; removing HHS info from mailers

--- app/mailers/export_mailer.rb
+++ app/mailers/export_mailer.rb
@@ -1,7 +1,7 @@
 # Sends a notification email upon completion of SurveyResponse export processing.
 # The email provides the recipient with a link to download the export file.
 class ExportMailer < ActionMailer::Base
-  default :from => "notifier@comment-adm.hhs.gov"
+  default :from => "notifier@#{smtp_settings[:domain] || default_url_options[:host]}"
 
   # Send an email containing a download link for the provided export file.
   # @param [Array<string>] emails an array of email recipients for the notification
@@ -9,6 +9,6 @@ class ExportMailer < ActionMailer::Base
   def export_download(emails, export_id)
       @export_file = Export.find export_id
 
-      mail to: emails, subject: "HHS VOC Tool - Export Download"
+      mail to: emails, subject: "VOC Tool - Export Download"
   end
 end
