939aaac91e89f1cdf444a21383b0f0fc72ad18fc
Jake Vose <jvose@ctacorp.com>
2013-04-22 15:44:03 +0000

    integrating subpath changes into trunk (with sso)

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
-require File.expand_path('../boot', __FILE__)
+# Set RAILS_RELATIVE_URL_ROOT if not installing as root application on a domain
+#ENV['RAILS_RELATIVE_URL_ROOT'] = "/vocsub"
 
+require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # If you have a Gemfile, require the gems listed there, including any gems
@@ -46,5 +48,6 @@ module CommentToolApp
 
     # This is suppose to cause the dig tag "field with errors" from breaking the styling.
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| "#{html_tag}".html_safe }
+    # config.action_controller.relative_url_root = '/vocsub'
   end
 end
