1418923a9d87ce535f3247d4718e4ecd0fad51f3
Jake Vose <jvose@ctacorp.com>
2013-05-22 16:28:29 -0400

    setting redis queue to be a class method to support inheritance; modifying module to properly include and extend; DisplayField.populate_default_values! is now round-tripping properly

--- lib/resque_async_runner.rb
+++ lib/resque_async_runner.rb
@@ -1,12 +1,21 @@
 module ResqueAsyncRunner
-	# This will be called by a worker when a job needs to be processed
-	def self.perform(id, method, *args)
-		find(id).send(method, *args)
-	end
+  def self.included base
+    base.send :include, InstanceMethods
+    base.extend ClassMethods
+  end
 
+  module InstanceMethods
 	# We can pass this any class instance method that we want to
 	# run later.
 	def async(method, *args)
 		Resque.enqueue(self.class, id, method, *args)
 	end
+  end
+
+  module ClassMethods
+	# This will be called by a worker when a job needs to be processed
+	def perform(id, method, *args)
+		find(id).send(method, *args)
+	end
+  end
 end
