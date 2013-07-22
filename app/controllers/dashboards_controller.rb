4d4cf3f08d9a836fc29abc10db0302cd2a0551f3
Angel N. Sciortino <asciortino@ctacorp.com>
2013-07-22 16:15:48 -0500

    Dashboard new and edit views

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,12 +2,15 @@
 #
 # Manages the Dashboard lifecycle.
 class DashboardsController < ApplicationController
+  before_filter :get_survey_version
+
   # GET    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards(.:format)
   def index
   end
 
   # GET    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/new(.:format)
   def new
+    @dashboard = @survey_version.dashboards.build
   end
 
   # POST   /surveys/:survey_id/survey_versions/:survey_version_id/dashboards(.:format)
@@ -16,6 +19,7 @@ class DashboardsController < ApplicationController
 
   # GET    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/:id/edit(.:format)
   def edit
+    @dashboard = Dashboard.find(params[:id])
   end
 
   # PUT    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/:id(.:format)
