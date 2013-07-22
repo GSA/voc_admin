f1738960c47ac0c3e2a3558e99d39656842c9690
Jake Vose <jvose@ctacorp.com>
2013-07-22 09:52:24 -0400

    adding name validations on report/dashboard models

--- app/models/reporting_models/dashboard_models/dashboard.rb
+++ app/models/reporting_models/dashboard_models/dashboard.rb
@@ -1,4 +1,6 @@
 class Dashboard < ActiveRecord::Base
   belongs_to :survey_version
   has_many :dashboard_elements
+
+  validates :name, :presence => true
 end
--- app/models/reporting_models/report_models/report.rb
+++ app/models/reporting_models/report_models/report.rb
@@ -1,4 +1,6 @@
 class Report < ActiveRecord::Base
   belongs_to :survey_version
   has_many :report_elements
+
+  validates :name, :presence => true
 end
