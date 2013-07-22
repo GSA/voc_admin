16e4cbda4b0c84f242d42f2c9d5d10172107002d
Angel N. Sciortino <asciortino@ctacorp.com>
2013-07-22 14:49:00 -0500

    Add skeleton CRUD controllers for dashboards and reports

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,28 @@
+# @author Communication Training Analysis Corporation <info@ctacorp.com>
+#
+# Manages the Dashboard lifecycle.
+class DashboardsController < ApplicationController
+  # GET    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards(.:format)
+  def index
+  end
+
+  # GET    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/new(.:format)
+  def new
+  end
+
+  # POST   /surveys/:survey_id/survey_versions/:survey_version_id/dashboards(.:format)
+  def create
+  end
+
+  # GET    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/:id/edit(.:format)
+  def edit
+  end
+
+  # PUT    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/:id(.:format)
+  def update
+  end
+
+  # DELETE /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/:id(.:format)
+  def destroy
+  end
+end
