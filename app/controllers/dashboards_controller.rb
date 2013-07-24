8ec19d95c2c94e8db2a35d775e380a8ea01e8c73
Jake Vose <jvose@ctacorp.com>
2013-07-24 09:11:50 -0400

    wiring up some more CRUD

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,10 +3,12 @@
 # Manages the Dashboard lifecycle.
 class DashboardsController < ApplicationController
   before_filter :get_survey_version
+  before_filter :get_dashboard
 
   # GET    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards(.:format)
-  def index
-  end
+  # def index
+  #   @dashboards = @survey_version.dashboards
+  # end
 
   # GET    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/new(.:format)
   def new
@@ -15,18 +17,42 @@ class DashboardsController < ApplicationController
 
   # POST   /surveys/:survey_id/survey_versions/:survey_version_id/dashboards(.:format)
   def create
+    @dashboard = @survey_version.dashboards.build params[:dashboard]
+
+    if @dashboard.save
+      redirect_to reporting_survey_survey_version_path(@survey, @survey_version), :notice  => "Successfully created dashboard."
+    else
+      render :new
+    end
+  end
+
+  # GET    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/:id(.:format)
+  def show
   end
 
   # GET    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/:id/edit(.:format)
   def edit
-    @dashboard = Dashboard.find(params[:id])
   end
 
   # PUT    /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/:id(.:format)
   def update
+    if @dashboard.update_attributes(params[:dashboard])
+      redirect_to survey_survey_version_dashboard_path(@survey, @survey_version, @dashboard), :notice  => "Successfully updated dashboard."
+    else
+      render :edit
+    end
   end
 
   # DELETE /surveys/:survey_id/survey_versions/:survey_version_id/dashboards/:id(.:format)
   def destroy
+    @dashboard.destroy
+
+    redirect_to survey_survey_version_dashboards_path(@survey, @survey_version), :notice  => "Successfully deleted dashboard."
+  end
+
+  private
+
+  def get_dashboard
+    @dashboard = @survey_version.dashboards.find(params[:id])
   end
 end
