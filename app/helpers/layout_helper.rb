d2fd4a1cb29dbe1012f31ebfb2e06098982bf04b
Jake Vose <jvose@ctacorp.com>
2013-07-29 13:39:53 -0400

    merging configurable template work into master (SSO version)

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -37,4 +37,35 @@ module LayoutHelper
 	    end
 	  end
   end
+
+  def branding_config(property)
+    APP_CONFIG['branding'].try(:[], property)
+  end
+
+  # Reads the stylesheet name from app_config.yml or provides the default value
+  #
+  # @return [String] configured stylesheet value or "application"
+  def main_stylesheet_location
+    branding_config('css-filename') || 'application'
+  end
+
+  # Reads the header partial name from app_config.yml or provides the default value;
+  # prepends "layouts/" to the string.  E.g. configured value "our_voc_header" returns
+  # "layouts/our_voc_header" and corresponds to /app/vuews/layouts/_our_voc_header.html.erb
+  # (note the underscore in the .erb partial view filename)
+  #
+  # @return [String] configured header partial location or "layouts/voc_header"
+  def header_partial_location
+    "layouts/#{branding_config('header-layout') || 'voc_header'}"
+  end
+
+  # Reads the footer partial name from app_config.yml or provides the default value;
+  # prepends "layouts/" to the string.  E.g. configured value "our_voc_footer" returns
+  # "layouts/our_voc_footer" and corresponds to /app/vuews/layouts/_our_voc_footer.html.erb
+  # (note the underscore in the .erb partial view filename)
+  #
+  # @return [String] configured footer partial location or "layouts/voc_footer"
+  def footer_partial_location
+    "layouts/#{branding_config('footer-layout') || 'voc_footer'}"
+  end
 end
