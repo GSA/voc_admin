657626ff1c955e76d79ce7ead6c03bc633ef2815
Jake Vose <jvose@ctacorp.com>
2013-07-30 15:46:40 -0400

    ran ruby beautifier to fix my janky tabs

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,34 +1,34 @@
 module DashboardsHelper
-	
-	# Links the JSON data from the ChoiceQuestionReporter model
-	# to the JQuery plugin function call.
-	def generatePieChart(chart)
-%Q[
+
+  # Links the JSON data from the ChoiceQuestionReporter model
+  # to the JQuery plugin function call.
+  def generatePieChart(chart)
+    %Q[
 	var data_#{chart.id} = #{chart.pie_chart_data};
 
 	$.plot("#pieChart_#{chart.id}", data_#{chart.id}, {
-			series: {
-				pie: {
-					show: true
-				}
-			},
-			grid: {
-				hoverable: true
-			},
-			legend: {
-				show: true,
-				labelFormatter: function(label, series) {
-					return label + ' (' + series.data[0][1] + ')';
-				}
-			}
-		});
+             series: {
+               pie: {
+                 show: true
+               }
+             },
+             grid: {
+               hoverable: true
+             },
+             legend: {
+               show: true,
+               labelFormatter: function(label, series) {
+                 return label + ' (' + series.data[0][1] + ')';
+               }
+             }
+        });
 ]
-	end
+  end
 
-	# Spool through all charts and generate JS blocks for each
-	def generatePieChartsJS(charts) 
-		charts.map { |chart| generatePieChart(chart) }.join("\n").html_safe
-	end
+  # Spool through all charts and generate JS blocks for each
+  def generatePieChartsJS(charts)
+    charts.map { |chart| generatePieChart(chart) }.join("\n").html_safe
+  end
 
   def dashboard_element_types_arr
     DashboardElement::ELEMENT_TYPES.map {|k,v| [v, k]}
