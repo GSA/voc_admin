ca3856ccdadcfe1ccd5790952d56929f9bbed52b
Angel N. Sciortino <asciortino@ctacorp.com>
2013-07-30 09:33:01 -0500

    WIP adding a widget on a new dashboard page

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -13,4 +13,15 @@ $(document).ready(function() {
 
     $('#dashboardModalShownDiv').show();
   });
+
+  function addWidgetToDashboard() {
+    var li = document.createElement("li");
+    var se_id = document.create_element("input");
+    se_id.attr("type", "hidden");
+    se_id.attr("name", "dashboard[dashboard_elements_attributes][][survey_element_id]");
+    se_id.attr("value", $("#dashboardModalShownDiv #survey_element_id option:selected").val());
+    var se_text = $("#dashboardModalShownDiv #survey_element_id option:selected").text();
+    li.append(se_id, se_text);
+    $("#dashboardElementsList").append(li);
+  };
 });
