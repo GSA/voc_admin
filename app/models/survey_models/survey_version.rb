f17292a03cb260c0c142d8ff990e2b422ae33647
Angel N. Sciortino <asciortino@ctacorp.com>
2013-08-02 13:48:02 -0500

    Add redis counter visits to SurveyVersion

--- app/models/survey_models/survey_version.rb
+++ app/models/survey_models/survey_version.rb
@@ -5,6 +5,7 @@ require 'csv'
 # A SurveyVersion is a working copy of a survey.  Only one version may be published (and
 # therefore collecting responses from the public site application) at a time.
 class SurveyVersion < ActiveRecord::Base
+  include Redis::Objects
   include ResqueAsyncRunner
   @queue = :voc_csv
 
@@ -44,6 +45,8 @@ class SurveyVersion < ActiveRecord::Base
 
   # Add methods to access the name and description of a survey from a version instance
   delegate :name, :description, :to => :survey, :prefix => true
+
+  counter :visits
 
   NOSQL_BATCH = 1000
 
