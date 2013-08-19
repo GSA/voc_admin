4465a8cdbc41ad8c52a808a081dfe64db24fd293
Angel N. Sciortino <asciortino@ctacorp.com>
2013-08-19 10:10:24 -0500

    Add method to SurveyVisitCount to give the count for a date range

--- app/models/survey_models/survey_visit_count.rb
+++ app/models/survey_models/survey_visit_count.rb
@@ -3,6 +3,12 @@
 # Represents a visit count on a date for a survey version.
 class SurveyVisitCount < ActiveRecord::Base
   belongs_to :survey_version
+
+  def self.count_for_date_range(start_date, end_date)
+    start_date = start_date.to_date unless start_date.is_a?(String)
+    end_date = end_date.to_date unless end_date.is_a?(String)
+    where(:visit_date => start_date..end_date).sum(:visits)
+  end
 end
 
 # == Schema Information
