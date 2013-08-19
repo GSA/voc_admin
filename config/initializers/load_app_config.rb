2af6a64077cea573008b3ad9ef6e3a2f6c329228
Angel N. Sciortino <asciortino@ctacorp.com>
2013-08-19 13:32:01 -0500

    Redis uses namespace of voc_{first letter of env}

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -1,5 +1,6 @@
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/app_config.yml")[Rails.env]
 
 Resque.redis = APP_CONFIG['redis'] || "localhost:6379"
-Redis.current = Resque.redis.redis
+Resque.redis.namespace = "voc_#{Rails.env[0]}"
+Redis.current = Redis::Namespace.new(Resque.redis.namespace, :redis => Resque.redis.redis)
 Resque::Plugins::Status::Hash.expire_in = (APP_CONFIG['redis-expire'] || "86400" ).to_i
