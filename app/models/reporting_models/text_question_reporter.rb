47fe49547a332729c3fbc53a887dfc1fccb5502e
Angel N. Sciortino <asciortino@ctacorp.com>
2013-09-16 09:35:30 -0500

    More common words to exclude in word cloud

--- app/models/reporting_models/text_question_reporter.rb
+++ app/models/reporting_models/text_question_reporter.rb
@@ -1,20 +1,21 @@
 class TextQuestionReporter < QuestionReporter
+  include ActionView::Helpers::NumberHelper
 
   field :tq_id, type: Integer    # TextQuestion id
   field :question, type: String
 
   COMMON_WORDS = %w(
-      a actually after afterwards albeit all also althoughhowever an and another
+      a actually after afterwards albeit all also althoughhowever am an and another
       any anybody anything are aren't as at be because before besides both but by can com concerning
-      considering conversely each either equally especially eventually everybody
-      everyone everything excluding few finally for further furthermore he he's hence
+      considering conversely did didn't does doesn't each either equally especially eventually everybody
+      everyone everything excluding few finally for further furthermore has hasn't have haven't he he's hence
       her hers herself him himself his however http i if i'm in incidentally including
       indeed initially instead into is it it's its itself lastly lest like likewise
-      many maybe me mine more moreover most much myself namely neither next nobody none
+      many maybe me mine more moreover most much my myself namely neither next no nobody none
       nor not notably nothing of off on one oneself onto or otherwise ours ourselves
       particularly previously rather regarding she she's similarly since so someone
       something specifically still than that the their theirs them themselves
-      then therefore these they this those though thus to too unless us via we what
+      then therefore these they this those though thus to too unless us via was we what
       whatever which whichever while who whoever whom whomever whose will with won't you
       your yours yourself yourselves
   )
