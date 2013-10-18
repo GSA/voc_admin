086af3a2127ea9d56e4f082f44cee342be8d63e4
Angel N. Sciortino <asciortino@ctacorp.com>
2013-10-18 15:29:56 -0500

    WIP reports creation

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,16 +2,27 @@
 #
 # Manages the Report lifecycle.
 class ReportsController < ApplicationController
-  # GET    /surveys/:survey_id/survey_versions/:survey_version_id/reports(.:format)
-  def index
-  end
+  before_filter :get_survey_version
+  before_filter :get_report, except: [:new, :create]
 
   # GET    /surveys/:survey_id/survey_versions/:survey_version_id/reports/new(.:format)
   def new
+    @report = @survey_version.reports.build
   end
 
   # POST   /surveys/:survey_id/survey_versions/:survey_version_id/reports(.:format)
   def create
+    @report = @survey_version.reports.build params[:report]
+
+    if @report.save
+      redirect_to survey_survey_version_report_path(@survey, @survey_version, @report), :notice  => "Successfully created report."
+    else
+      render :new
+    end
+  end
+
+  # GET    /surveys/:survey_id/survey_versions/:survey_version_id/reports/:id(.:format)
+  def show
   end
 
   # GET    /surveys/:survey_id/survey_versions/:survey_version_id/reports/:id/edit(.:format)
@@ -20,9 +31,23 @@ class ReportsController < ApplicationController
 
   # PUT    /surveys/:survey_id/survey_versions/:survey_version_id/reports/:id(.:format)
   def update
+    if @report.update_attributes(params[:report])
+      redirect_to survey_survey_version_report_path(@survey, @survey_version, @report), :notice  => "Successfully updated report."
+    else
+      render :edit
+    end
   end
 
   # DELETE /surveys/:survey_id/survey_versions/:survey_version_id/reports/:id(.:format)
   def destroy
+    @report.destroy
+
+    redirect_to reporting_survey_survey_version_path(@survey, @survey_version), :notice  => "Successfully deleted report."
+  end
+
+  private
+
+  def get_report
+    @report = @survey_version.reports.find(params[:id])
   end
 end
