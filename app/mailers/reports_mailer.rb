004b38a40e0f5e5fe7834488fe82e0dd1b81709a
Angel N. Sciortino <asciortino@ctacorp.com>
2013-11-04 14:36:32 -0600

    Report mailer updates. Combine shared code and add html.

--- app/mailers/reports_mailer.rb
+++ app/mailers/reports_mailer.rb
@@ -7,23 +7,30 @@ class ReportsMailer < ActionMailer::Base
   default :from => "notifier@#{default_url_options[:host] || smtp_settings[:domain]}"
 
   def report_csv(report_id, emails, from_user, report_frequency)
-    @report = Report.find(report_id)
-    @from_user = from_user
-    @report_frequency = report_frequency
+    shared_settings(report_id, from_user, report_frequency)
     attachments["report_#{@report.id}.csv"] = {:mime_type => 'text/csv', :content => @report.to_csv}
+    @report_type = 'CSV'
     mail :to => emails, 
-         :subject => "Report CSV: #{@report.name}"
+         :subject => "Report CSV: #{@report.name}",
+         :template_name => "report"
   end
 
   def report_pdf(report_id, emails, from_user, report_frequency)
-    @report = Report.find(report_id)
-    @from_user = from_user
-    @report_frequency = report_frequency
+    shared_settings(report_id, from_user, report_frequency)
     token, salt = token_and_salt
     url = survey_survey_version_pdf_report_url(@report.survey_version.survey_id, @report.survey_version_id, @report, :format => :pdf)
     file = open("#{url}?token=#{token}&salt=#{salt}").read
     attachments["report_#{@report.id}.pdf"] = {:mime_type => 'application/pdf', :content => file}
+    @report_type = 'PDF'
     mail :to => emails, 
-         :subject => "Report PDF: #{@report.name}"
+         :subject => "Report PDF: #{@report.name}",
+         :template_name => "report"
+  end
+
+  private
+  def shared_settings(report_id, from_user, report_frequency)
+    @report = Report.find(report_id)
+    @from_user = from_user
+    @report_frequency = report_frequency
   end
 end
