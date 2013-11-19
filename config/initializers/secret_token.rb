aa83682386c6c63dda96c93363afe5e1ea98e106
Jake Vose <jvose@ctacorp.com>
2013-11-19 09:42:50 -0500

    refactoring secret_token out to a YAML to “unhide” it from an initializers/*.rb file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,15 @@
-# Be sure to restart your server when you modify this file.
+secret_token_loaded = false
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CommentToolApp::Application.config.secret_token = '04b43dc045ad813768144d4aa6adfe1e36b11f147d05b043f917855a54b8cd66b2421f50b38c7c842dd48e5546ce86d6c2510b7af7c6d1b2fa45718b08a73802'
+begin
+  SecretConfig = YAML.load_file("#{Rails.root}/config/secret_settings.yml")
+
+  unless SecretConfig.blank? or (token = SecretConfig["secret"]).nil?
+    CommentToolApp::Application.config.secret_token = token
+    secret_token_loaded = true
+  end
+rescue
+end
+
+unless secret_token_loaded
+  raise "Missing configuration file for secret key settings (config/secret_settings.yml) #{$!}"
+end
