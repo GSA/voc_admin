c4ffbae6e9995191ca296597d6260ee2e7fb2631
Angel N. Sciortino <asciortino@ctacorp.com>
2014-02-12 16:20:27 -0600

    Add resque worker scripts for Windows

---
+++ war_resque_workers.bat
@@ -0,0 +1,6 @@
+SET RAILS_ENV=production
+SET BUNDLE_WITHOUT=development:test
+SET BUNDLE_GEMFILE=Gemfile
+SET GEM_HOME=gems
+java -classpath "lib/*" org.jruby.Main -S rake resque:start_workers RAILS_ENV=production
+pause
