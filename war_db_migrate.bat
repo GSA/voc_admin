e52c9f2017840c98f70040c88d5231e572efa06a
Angel N. Sciortino <asciortino@ctacorp.com>
2014-02-12 16:05:38 -0600

    Update scripts for reporting

--- war_db_migrate.bat
+++ war_db_migrate.bat
@@ -2,5 +2,5 @@ SET RAILS_ENV=production
 SET BUNDLE_WITHOUT=development:test
 SET BUNDLE_GEMFILE=Gemfile
 SET GEM_HOME=gems
-java -classpath "lib/*" org.jruby.Main -S rake db:create db:migrate db:seed
+java -classpath "lib/*" org.jruby.Main -S rake db:create db:migrate db:seed db:mongoid:create_indexes
 pause
