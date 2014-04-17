bc747541a3cc98d5f6a2a2efd481f42a91f9c4a4
Juan Alvarado <jalvarado@ctacorp.com>
2014-04-17 07:42:46 -0400

    Allow editing of text questions

--- app/controllers/question_bank/text_questions_controller.rb
+++ app/controllers/question_bank/text_questions_controller.rb
@@ -15,6 +15,21 @@ class QuestionBank::TextQuestionsController < ApplicationController
     end
   end
 
+  def edit
+    @text_question = QuestionBank.instance.text_questions.find params[:id]
+  end
+
+  def update
+    @text_question = TextQuestion.find params[:id]
+    @text_question.question_content.skip_observer = true
+
+    if @text_question.update_attributes(params[:text_question])
+      redirect_to question_bank_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @text_question = TextQuestion.find params[:id]
     @text_question.destroy
