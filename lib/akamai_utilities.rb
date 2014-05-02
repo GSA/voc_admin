cbe6db280576da06f1e44eabf055015eb4acb65d
Dan Martin <dmartin@ctacorp.com>
2014-05-02 13:42:13 -0400

    add logging and survey_version url.

--- lib/akamai_utilities.rb
+++ lib/akamai_utilities.rb
@@ -1,16 +1,24 @@
   module AkamaiUtilities
-    def flush_akamai(survey_id)
-      unless AKAMAI_CONFIG['development_mode']
+    require 'logger'
+    def flush_akamai(survey_id, survey_version)
+
+      logger = Logger.new('log/Akamai.log')
+      logger.level = Logger::WARN
+
+       unless AKAMAI_CONFIG['development_mode']
         auth = {:username => AKAMAI_CONFIG['user_name'], :password => AKAMAI_CONFIG['password']}
         response = HTTParty.post(AKAMAI_CONFIG['base_uri'].to_str, :basic_auth => auth, :headers => { 'Content-Type' => 'application/json' }, :body => { :type => 'arl', 
-                :action => 'remove',
-                 :domain => AKAMAI_CONFIG['domain'], 
-                 :objects => ["http://#{APP_CONFIG['public_host']}/surveys/#{survey_id}".to_s]}.to_json)
+                 :action => 'remove',
+                  :domain => AKAMAI_CONFIG['domain'], 
+                 :objects => ["http://#{APP_CONFIG['public_host']}/surveys/#{survey_id}".to_s,
+                              "http://#{APP_CONFIG['public_host']}/surveys/#{survey_id}?version={survey_version}".to_s]}.to_json)
         parsed_response = JSON.parse(response.body)
         httpresponse = parsed_response['httpStatus']
+        logger.warn(Time.now)
+        logger.warn(parsed_response)
 
         httpresponse == 201
-      end
+        end
     end
   end
 
