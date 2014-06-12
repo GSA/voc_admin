c768e045cc4997406a380151fa7b36092cf2a5d6
Juan Alvarado <jalvarado@ctacorp.com>
2014-06-12 07:31:03 -0400

    use brew services to start/stop mysql, mongo, and redis

--- bin/stop_all.sh
+++ bin/stop_all.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 # Stop all databases required for project
-
-bin_path=$( cd $(dirname $0) ; pwd -P )
-
-echo $( ${bin_path}/mongod.sh stop )
-echo $( ${bin_path}/redis.sh stop )
-
-bundle exec rake resque:stop_workers
-echo "Successfully stopped resque workers."
+brew services stop redis
+brew services stop mongodb
+brew services stop mysql
