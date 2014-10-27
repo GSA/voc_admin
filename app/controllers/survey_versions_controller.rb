6c24f3019c8b9589daf9c3d8ca60fabab2fd8a89
kchin_ctac <kchin@ctacorp.com>
2014-10-27 10:59:11 -0400

    GSA-162 Added processed to filter failed responses

--- app/controllers/survey_versions_controller.rb
+++ app/controllers/survey_versions_controller.rb
@@ -99,7 +99,7 @@ class SurveyVersionsController < ApplicationController
       @first_count_date = Date.parse(temp_visit) unless temp_visit.nil?
     end  
 
-    @response_count = @survey_version.survey_responses.where("created_at >= ?", @first_count_date).count
+    @response_count = @survey_version.survey_responses.processed.where("created_at >= ?", @first_count_date).count
     @response_rate = 0
     if @survey_version.total_visit_count > 0
       @response_rate = @response_count * 100.0 / @survey_version.total_visit_count
