9ef9d19e8965a94c6cfbbf923de408c10603347c
kchin_ctac <kchin@ctacorp.com>
2014-10-27 09:29:43 -0400

    GSA-162 Fixed Response numbers on reporting after reset count rake task.

--- app/controllers/survey_versions_controller.rb
+++ app/controllers/survey_versions_controller.rb
@@ -93,6 +93,12 @@ class SurveyVersionsController < ApplicationController
   # GET    /surveys/:survey_id/survey_versions/:id/reporting(.:format)
   def reporting
     @first_count_date = @survey_version.survey_version_counts.where("visits > 0").minimum(:count_date)
+    
+    if @first_count_date.nil? 
+      temp_visit = @survey_version.temp_visit_count.each_key.min_by{|k| k} 
+      @first_count_date = Date.parse(temp_visit) unless temp_visit.nil?
+    end  
+
     @response_count = @survey_version.survey_responses.where("created_at >= ?", @first_count_date).count
     @response_rate = 0
     if @survey_version.total_visit_count > 0
