6420e33ef338350e698fcc81d22c062cb90b98cd
Juan Alvarado <jalvarado@ctacorp.com>
2014-10-28 09:41:58 -0400

    Allow for old responses to be processes if they are still in the queue when the update is done

--- app/models/survey_models/survey_response_create_job.rb
+++ app/models/survey_models/survey_response_create_job.rb
@@ -3,6 +3,10 @@ class SurveyResponseCreateJob
 
   # Wrapper for Resque job worker
   def self.perform(raw_submission_id, survey_version_id)
-    ProcessRawSubmission.process(raw_submission_id, survey_version_id)
+    if raw_submission_id.is_a? Hash
+      SurveyResponse.process_response(raw_submission_id, survey_version_id)
+    else
+      ProcessRawSubmission.process(raw_submission_id, survey_version_id)
+    end
   end
 end
