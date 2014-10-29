3b5873966c35a3be298432605f03d1940900038b
Angel N. Sciortino <asciortino@ctacorp.com>
2014-10-29 09:39:03 -0500

    WCD-2884 Sorting the index works!!!

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -11,29 +11,44 @@ unless ELASTIC_SEARCH_CLIENT.indices.exists index: 'survey_responses'
   ELASTIC_SEARCH_CLIENT.indices.create index: 'survey_responses', body: {
     mappings: {
       "_default_" => {
-        dynamic_templates: {
-          str: {
-            match: "^qc_.*|^df_.*|^page_url$|^device$",
-            match_mapping_type: "string",
-            match_pattern: "regex",
-            mapping: {
-              type: "string",
-              fields: {
-                raw: {type: "string", index: "not_analyzed"}
+        dynamic_templates: [
+          {
+            str: {
+              match: "^qc_.*|^df_.*|^page_url$|^device$",
+              match_mapping_type: "string",
+              match_pattern: "regex",
+              mapping: {
+                type: "string",
+                fields: {
+                  raw: {type: "string", index: "not_analyzed"}
+                }
               }
             }
           },
-          created_at: {
-            match: "created_at",
-            match_mapping_type: "dateOptionalTime",
-            mapping: {
-              type: "dateOptionalTime",
-              fields: {
-                raw: {type: "dateOptionalTime", index: "not_analyzed"}
+          {
+            created_at: {
+              match: "created_at",
+              match_mapping_type: "date",
+              mapping: {
+                type: "date",
+                format: "dateOptionalTime",
+                fields: {
+                  raw: {type: "date", format: "dateOptionalTime", index: "not_analyzed"}
+                }
+              }
+            }
+          },
+          {
+            ids: {
+              match: "^survey.*id$",
+              match_mapping_type: "long",
+              match_pattern: "regex",
+              mapping: {
+                type: "long"
               }
             }
           }
-        }
+        ]
       }
     }
   }
