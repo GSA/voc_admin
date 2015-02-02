1e33cbacb03b001980bee56b22b73122d1af1e2d
Juan Alvarado <jalvarado@ctacorp.com>
2015-02-02 08:39:56 -0500

    Address issues with date field search

--- app/models/reporting_models/report_models/elasticsearch_advanced_search.rb
+++ app/models/reporting_models/report_models/elasticsearch_advanced_search.rb
@@ -8,13 +8,23 @@ class ElasticsearchAdvancedSearch
   def filter_object
     @search_params['criteria'].map do |k, criteria|
       value = criteria['value']
-      value = pase_date_in_local_time(value) if criteria['display_field_id'] == "created_at"
+      column = criteria['display_field_id']
+      date_search = column == 'created_at'
+      time_string = '%m/%d/%Y' if /\d{1,2}\/\d{1,2}\/\d{4}/.match(value)
+      value = parse_date_in_local_time(value) if date_search
       [
         criteria['clause_join'],
-        CONDITIONS[criteria['condition']].call(
-          criteria['display_field_id'],
-          value
+        (date_search ?
+          DATE_CONDITIONS[criteria['condition']].call(
+            column,
+            value,
+            time_string
+          ) : CONDITIONS[criteria['condition']].call(
+            column,
+            value
+          )
         )
+
       ]
     end.inject({}) {|filters, f|
       if f.first == nil
@@ -25,9 +35,55 @@ class ElasticsearchAdvancedSearch
     }
   end
 
+  DATE_CONDITIONS = {
+    "equals" => proc {|column, value, time_string|
+      case time_string
+      when "%m/%d/%Y"
+        {
+          "range" => {
+            column => {
+              "gte" => value.beginning_of_day,
+              "lte" => value.end_of_day
+            }
+          }
+        }
+      else
+        {
+          "query" => {
+            "match" => { "#{column}.raw" => value }
+          }
+        }
+      end
+    },
+    "greater_than" => proc {|column, value, time_string|
+      case time_string
+      when "%m/%d/%Y"
+        {
+          "range" => { column => { "gt" => value.end_of_day } }
+        }
+      else
+        {
+          "range" => { column => { "gt" => value } }
+        }
+      end
+    },
+    "less_than" => proc {|column, value, time_string|
+      case time_string
+      when "%m/%d/%Y"
+        {
+          "range" => { column => { "lt" => value.beginning_of_day } }
+        }
+      else
+        {
+          "range" => { column => { "lt" => value } }
+        }
+      end
+    }
+
+  }
+
   CONDITIONS = {
     "equals" => proc {|column, value|
-
       {
         "query" => {
           "match" => { "#{column}.raw" => value }
@@ -89,24 +145,24 @@ class ElasticsearchAdvancedSearch
     end
   end
 
-  def pase_date_in_local_time(value)
+  def parse_date_in_local_time(value)
     time_string = nil
     # chomp the value
     value.chomp!
-
-    # Set the time format string based on the datetime input by the  user
-    if /\d{1,2}\/\d{1,2}\/\d{4}\s\d{2}:\d{2}:\d{2}/.match(value)
-      time_string = "%m/%d/%Y %H:%M:%S"
-    elsif /\d{1,2}\/\d{1,2}\/\d{4}\s\d{2}:\d{2}/.match(value)
-      time_string = "%m/%d/%Y %H:%M"
-    elsif /\d{1,2}\/\d{1,2}\/\d{4}/.match(value)
-      time_string = '%m/%d/%Y'
-    else
-      # Did not match a time format.  Pass on the string value.
-      return value
-    end
-
     begin
+      # Set the time format string based on the datetime input by the  user
+      if /\d{1,2}\/\d{1,2}\/\d{4}\s\d{2}:\d{2}:\d{2}/.match(value)
+        time_string = "%m/%d/%Y %H:%M:%S"
+      elsif /\d{1,2}\/\d{1,2}\/\d{4}\s\d{2}:\d{2}/.match(value)
+        time_string = "%m/%d/%Y %H:%M"
+      elsif /\d{1,2}\/\d{1,2}\/\d{4}/.match(value)
+        time_string = '%m/%d/%Y'
+        return Date.strptime(value, time_string)
+      else
+        # Did not match a time format.  Pass on the string value.
+        return value
+      end
+
       DateTime.strptime(value + " #{Time.zone.now.formatted_offset}", time_string.chomp + " %:z").to_s
     rescue
       value
