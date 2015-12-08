73c439f52a9caf9e2725d1b1cc5334759209fcf9
Juan Alvarado <jalvarado@ctacorp.com>
2015-12-08 11:00:36 -0500

    turn off debug mode for Capybara

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,5 +2,5 @@ Capybara.javascript_driver = :webkit
 
 Capybara::Webkit.configure do |config|
   # Enable debug mode. Prints a log of everything the driver is doing.
-  config.debug = true
+  #config.debug = true
 end
