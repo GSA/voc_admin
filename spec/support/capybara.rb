0a313fbe30ea9b9150651a8440b3edb8332a3c7b
Juan Alvarado <jalvarado@ctacorp.com>
2015-12-08 13:16:01 -0500

    Add asset host for tests

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,4 +1,5 @@
 Capybara.javascript_driver = :webkit
+Capybara.asset_host = "http://localhost:3000"
 
 Capybara::Webkit.configure do |config|
   # Enable debug mode. Prints a log of everything the driver is doing.
