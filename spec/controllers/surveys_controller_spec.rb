49ce218c8bdfcca80b92cca4adfdaa54096af813
Juan Alvarado <jalvarado@ctacorp.com>
2015-12-22 16:10:48 -0500

    Refactor old tests

--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -130,8 +130,134 @@ RSpec.describe SurveysController, type: :controller do
     end
   end
 
+  context "PATCH /update", "#update" do
+    it "assigns @survey" do
+      site = FactoryGirl.create :site
+      survey = FactoryGirl.create :survey, site_id: site.id
+      login_user user:FactoryGirl.create(:user, site_ids: [site.id])
+      patch :update, id: survey.id
+      expect(assigns(:survey)).to be_a(Survey)
+    end
+
+    context "with valid attributes" do
+      it "updates the survey in the database" do
+        site = FactoryGirl.create :site
+        survey = FactoryGirl.create :survey, site_id: site.id
+        valid_attributes = { name: "updated" }
+        login_user user: FactoryGirl.create(:user, site_ids: [site.id])
+        expect { patch :update, id: survey.id, survey: valid_attributes }
+          .to change { Survey.find(survey.id).name }
+      end
+      it "redirects to the surveys index" do
+        site = FactoryGirl.create :site
+        survey = FactoryGirl.create :survey, site_id: site.id
+        valid_attributes = { name: "updated" }
+        login_user user: FactoryGirl.create(:user, site_ids: [site.id])
+        patch :update, id: survey.id, survey: valid_attributes
+        expect(response).to redirect_to surveys_path
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not update the survey in the database" do
+        site = FactoryGirl.create :site
+        survey = FactoryGirl.create :survey, site_id: site.id
+        invalid_attributes = { name: "" }
+        login_user user: FactoryGirl.create(:user, site_ids: [site.id])
+        expect { patch :update, id: survey.id, survey: invalid_attributes }
+          .to_not change { Survey.find(survey.id).name }
+      end
+      it "renders the edit template" do
+        site = FactoryGirl.create :site
+        survey = FactoryGirl.create :survey, site_id: site.id
+        invalid_attributes = { name: "" }
+        login_user user: FactoryGirl.create(:user, site_ids: [site.id])
+        patch :update, id: survey.id, survey: invalid_attributes
+        expect(response).to render_template("edit")
+      end
+    end
+  end
+
+  context "DELETE /destroy", "#destroy" do
+    it "sets the archived flag for the survey" do
+      survey = FactoryGirl.create :survey
+      login_user user: FactoryGirl.create(:user, site_ids: [survey.site_id])
+      delete :destroy, id: survey.id
+      survey.reload
+      expect(survey.archived).to be true
+      expect(response).to redirect_to surveys_path
+    end
+  end
+
+  context "GET /start_page_preview", "#start_page_preview" do
+    it "assigns @survey" do
+      survey = FactoryGirl.create :survey
+      login_user user: FactoryGirl.create(:user, site_ids: [survey.site_id])
+      get :start_page_preview, id: survey.id
+      expect(assigns(:survey)).to be_a Survey
+    end
+    it "renders the start_page_preview template" do
+      survey = FactoryGirl.create :survey
+      login_user user: FactoryGirl.create(:user, site_ids: [survey.site_id])
+      get :start_page_preview, id: survey.id
+      expect(response).to render_template("start_page_preview")
+      expect(response).to_not render_template(layout: "application")
+    end
+  end
+
+  context "GET /all_questions", "#all_questions" do
+    it "redirects to the surveys index when user is not an admin" do
+      login_user user: FactoryGirl.create(:user)
+      get :all_questions
+      expect(response).to redirect_to surveys_path
+    end
+    it "shows only published versions" do
+      published_version = FactoryGirl.create :survey_version, :published
+      login_user user: FactoryGirl.create(:user, role_id: Role::ADMIN.id)
+      get :all_questions
+      expect(assigns(:published_versions)).to eq [published_version]
+      expect(response).to render_template("all_questions")
+    end
+  end
+
+  context "POST /import_survey_version", "#import_survey_version" do
+    it "assigns @survey" do
+      survey = FactoryGirl.create :survey
+      login_user user: FactoryGirl.create(:user, site_ids: [survey.site_id])
+      post :import_survey_version, survey_id: survey.id
+      expect(assigns(:survey)).to eq survey
+    end
+
+    context "with valid import" do
+      it "calls import_survey_version on survey" do
+        survey = instance_spy(Survey)
+        allow(Survey).to receive(:find) {1}.and_return survey
+        file = fixture_file_upload("/survey_export.json", "application/json")
+        login_user user: FactoryGirl.create(:user)
+        expect(survey).to receive(:import_survey_version)
+        post :import_survey_version, survey_id: 1, file: file
+      end
+    end
+
+    context "with invalid import" do
+      it "redirects to the survey_versions list" do
+        survey = double(Survey)
+        allow(Survey).to receive(:find) {1}.and_return survey
+        login_user user: FactoryGirl.create(:user)
+        post :import_survey_version, survey_id: 1
+        expect(response).to redirect_to survey_survey_versions_path(survey)
+      end
+      it "sets an error message" do
+        survey = double(Survey)
+        allow(Survey).to receive(:find) {1}.and_return survey
+        login_user user: FactoryGirl.create(:user)
+        post :import_survey_version, survey_id: 1
+        expect(flash[:alert]).to eq "Please select a file before clicking the import button."
+      end
+    end
+  end
+
   def login_user user:
-    UserSession.find.try(:destroy)
     UserSession.create user, false
   end
 end
--- spec/factories/survey_versions.rb
+++ spec/factories/survey_versions.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
     major 1
     sequence(:minor)
     association(:survey)
+
+    trait :published do
+      published { true }
+    end
   end
 end
