e59fafa577a76423ae4279c347fbb25f7a5985c0
Juan Alvarado <jalvarado@ctacorp.com>
2015-12-30 11:31:29 -0500

    Add spec for adding a regular user

--- spec/features/admin_creates_a_new_user_spec.rb
+++ spec/features/admin_creates_a_new_user_spec.rb
@@ -9,6 +9,14 @@ RSpec.feature "Admin creates a new user" do
     expect(page).to have_content "Example User"
   end
 
+  scenario "without the admin role" do
+    login_user
+    click_link "Manage Users"
+    submit_user first_name: "Example", last_name: "User", email: "example@test.com",
+      hhs_id: "0123456789", role: "User", sites: [create(:site).name]
+    expect(page).to have_content "Example User"
+  end
+
   scenario "with invalid attributes" do
     login_user
     click_link "Manage Users"
@@ -18,13 +26,18 @@ RSpec.feature "Admin creates a new user" do
   end
 
 
-  def submit_user first_name:, last_name:, email:, hhs_id:, role:
+  def submit_user first_name:, last_name:, email:, hhs_id:, role:, sites: []
     click_link "New User", match: :first
     fill_in "First Name", with: first_name
     fill_in "Last Name", with: last_name
     fill_in "Email", with: email
     fill_in "HHS ID", with: hhs_id
     choose role
+    if role == "User"
+      sites.each do |site|
+        select site, from: "Sites"
+      end
+    end
     click_button "Create User"
   end
 end
