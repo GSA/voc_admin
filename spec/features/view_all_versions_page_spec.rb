df7afbd0ab551641ded14edefe6f60d33d711d52
Juan Alvarado <jalvarado@ctacorp.com>
2015-12-30 10:23:15 -0500

    Test survey version import

--- spec/features/view_all_versions_page_spec.rb
+++ spec/features/view_all_versions_page_spec.rb
@@ -24,4 +24,34 @@ RSpec.feature "View all versions page" do
     find(:css, ".col11 a").click
     expect(page).to have_content "1.1"
   end
+
+  scenario "User should be able to delete a version" do
+    survey = create :survey
+    login_user
+    visit survey_survey_versions_path(survey)
+    expect(page).to have_content("1.0")
+    find(:css, "a.deleteLink").click
+    expect(page).to_not have_content("1.0")
+  end
+
+  context "Survey version import" do
+    scenario "with a valid import file" do
+      survey = create :survey
+      login_user
+      visit survey_survey_versions_path(survey)
+      attach_file "file", "spec/fixtures/survey_export.json"
+      click_button "Import"
+      expect(page).to have_content "2.0"
+    end
+
+    scenario "with invalid import file" do
+      survey = create :survey
+      login_user
+      visit survey_survey_versions_path(survey)
+      attach_file "file", "spec/fixtures/invalid_import.json"
+      click_button "Import"
+      expect(page).to_not have_content "2.0"
+      expect(page).to have_content "Unable to import file."
+    end
+  end
 end
