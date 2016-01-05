c916fc1f31a5a99a8246fb77b736d98506be9899
Juan Alvarado <jalvarado@ctacorp.com>
2016-01-05 12:17:23 -0500

    replace link_to_function

--- spec/features/user_creates_a_new_display_field_spec.rb
+++ spec/features/user_creates_a_new_display_field_spec.rb
@@ -13,6 +13,19 @@ RSpec.feature "User creates a new display field", js: true do
     click_button "Create Display Field"
 
     expect(page).to have_css "tr", text: "Custom Display Field"
+  end
 
+  scenario "with invalid attributes" do
+    login_user
+    create_site
+    setup_survey name: "Example"
+    load_responses_for survey_name: "Example", version_number: "1.0"
+
+    click_link "Manage Display Fields", match: :first
+    click_link "Add New Display Field", match: :first
+    fill_in "Name", with: ""
+    click_button "Create Display Field"
+
+    expect(page).to have_content "can't be blank"
   end
 end
