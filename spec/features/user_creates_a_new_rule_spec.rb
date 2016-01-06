a2fad70162d3fbdd73b18487eddd5a3729ead59a
Juan Alvarado <jalvarado@ctacorp.com>
2016-01-05 22:59:06 -0500

    Add specs for creating a rule with multiple criteria

--- spec/features/user_creates_a_new_rule_spec.rb
+++ spec/features/user_creates_a_new_rule_spec.rb
@@ -2,52 +2,82 @@ require "rails_helper"
 
 RSpec.feature "User creates a new rule", js: true do
   scenario "with valid attributes" do
-    login_user
-    create_site
-    survey = setup_survey name: "Example"
-    load_responses_for survey_name: "Example", version_number: "1.0"
-    Conditional.create name: "="
-    FactoryGirl.create(:text_question,
-      survey_version: survey.survey_versions.first,
-      question_content: FactoryGirl.create(:question_content, statement: "Text Question")
-    )
+    create_test_survey_with_text_question statement: "Text Question"
 
-    click_link "Manage Rules", match: :first
-    click_link "Add New Rule", match: :first
-    fill_in "Name", with: "Custom Rule"
-    choose "db_action_rule"
-    check "Add"
-    select "Text Question(Question)", from: "Source select"
-    select "=", from: "Conditional"
-    fill_in "Value", with: "Test"
-    fill_in "value_text", with: "Test"
-    click_button "Create Rule"
+    add_rule name: "Custom Rule", action: "Database Action", trigger: "Add",
+      criteria: [{ source: "Text Question(Question)", conditional: "=", value: "Test" }],
+      actions: [{update: "Text Question", value: "Test"}]
+
 
     expect(page).to have_content "Custom Rule"
   end
 
+  scenario "with multiple criteria" do
+    create_test_survey_with_text_question statement: "Text Question"
+
+    add_rule name: "Custom Rule", action: "Database Action", trigger: "Add",
+      criteria: [
+        {source: "Text Question(Question)", conditional: "=", value: "Test"},
+        {source: "Text Question(Question)", conditional: ">", value: "Test"}
+      ],
+      actions: [{update: "Text Question", value: "Test"}]
+
+    expect(page).to have_css(".criteria li", text: "Test", count: 2)
+  end
+
   scenario "with invalid attributes" do
+    create_test_survey_with_text_question statement: "Text Question"
+
+    add_rule name: "", action: "Database Action", trigger: "Add",
+      criteria: [{ source: "Text Question(Question)", conditional: "=", value: "Test" }],
+      actions: [{update: "Text Question", value: "Test"}]
+
+    expect(page).to have_content "can't be blank"
+  end
+
+  def create_test_survey_with_text_question statement:
     login_user
     create_site
     survey = setup_survey name: "Example"
     load_responses_for survey_name: "Example", version_number: "1.0"
-    Conditional.create name: "="
+    Conditional.create! name: "="
+    Conditional.create! name: ">"
     FactoryGirl.create(:text_question,
       survey_version: survey.survey_versions.first,
-      question_content: FactoryGirl.create(:question_content, statement: "Text Question")
+      question_content: FactoryGirl.create(:question_content, statement: statement)
     )
+  end
 
+  def add_rule name:, action:, trigger:, criteria:, actions:
     click_link "Manage Rules", match: :first
     click_link "Add New Rule", match: :first
-    fill_in "Name", with: ""
-    choose "db_action_rule"
-    check "Add"
-    select "Text Question(Question)", from: "Source select"
-    select "=", from: "Conditional"
-    fill_in "Value", with: "Test"
-    fill_in "value_text", with: "Test"
-    click_button "Create Rule"
+    fill_in "Name", with: name
+    choose action
+    check trigger
 
-    expect(page).to have_content "can't be blank"
+    # Build the criteria objects
+    if criteria.size > 1
+      (criteria.size-1).times { click_link("Add Criteria") }
+    end
+    criteria.zip(page.all(:css, ".criterion")) do |criterion, fields|
+      within fields do
+        select criterion.fetch(:source), from: "Source select"
+        select criterion.fetch(:conditional), from: "Conditional"
+        fill_in "Value", with: criterion.fetch(:value)
+      end
+    end
+
+    # Build the action objects
+    if actions.size > 1
+      (actions.size-1).times { click_link("Add Action") }
+    end
+    actions.zip(page.all(:css, ".db_action")) do |action, fields|
+      within fields do
+        select action.fetch(:update), from: "Update"
+        fill_in "value_text", with: action.fetch(:value)
+      end
+    end
+
+    click_button "Create Rule"
   end
 end
