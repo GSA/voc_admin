6f7301cae942d5a4cc4a8f4481a9e39b55b7c915
Juan Alvarado <jalvarado@ctacorp.com>
2016-01-14 10:18:06 -0500

    Fix best in place editing of custom display fields

--- app/controllers/display_field_values_controller.rb
+++ app/controllers/display_field_values_controller.rb
@@ -20,11 +20,11 @@ class DisplayFieldValuesController < ApplicationController
     @display_field_value = DisplayFieldValue.find(params[:id])
 
     respond_to do |format|
-      if @display_field_value.update_attributes(params[:display_field_value])
-        
+      if @display_field_value.update_attributes(display_field_value_attributes)
+
         # Reprocess response when a field is updated
         @display_field_value.survey_response.async(:process_me, 2)
-        
+
         format.html {redirect_to root_url, :notice  => "Successfully updated display field."}
         format.js
         format.json { head :ok }
@@ -34,5 +34,11 @@ class DisplayFieldValuesController < ApplicationController
         format.json { render :json => @display_field_value.errors.full_messages, :status => :unprocessable_entity }
       end
     end
+  end
+
+  private
+
+  def display_field_value_attributes
+    params.require(:display_field_value).permit(:value)
   end
 end
