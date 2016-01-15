1baad641d0a0920fdcfec7288f59a2904d1d6b40
Juan Alvarado <jalvarado@ctacorp.com>
2016-01-15 11:37:30 -0500

    Add mappings for the different environments

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -8,54 +8,65 @@ ELASTIC_SEARCH_CLIENT = Elasticsearch::Client.new({
   log: false
 })
 
-ELASTIC_SEARCH_MAPPINGS = {
-  survey_responses: {
-    mappings: {
-      "_default_" => {
-        dynamic_templates: [
-          {
-            str: {
-              match: "^qc_.*|^df_.*|^page_url$|^device$",
-              match_mapping_type: "string",
-              match_pattern: "regex",
-              mapping: {
-                type: "string",
-                fields: {
-                  raw: {type: "string", index: "not_analyzed"}
-                }
-              }
-            }
-          },
-          {
-            created_at: {
-              match: "created_at",
-              match_mapping_type: "date",
-              mapping: {
-                type: "date",
-                format: "dateOptionalTime",
-                fields: {
-                  raw: {type: "date", format: "dateOptionalTime", index: "not_analyzed"}
-                }
-              }
-            }
-          },
-          {
-            ids: {
-              match: "^survey.*id$",
-              match_mapping_type: "long",
-              match_pattern: "regex",
-              mapping: {
-                type: "long"
-              }
+mappings = {
+  "_default_" => {
+    dynamic_templates: [
+      {
+        str: {
+          match: "^qc_.*|^df_.*|^page_url$|^device$",
+          match_mapping_type: "string",
+          match_pattern: "regex",
+          mapping: {
+            type: "string",
+            fields: {
+              raw: {type: "string", index: "not_analyzed"}
             }
           }
-        ]
+        }
+      },
+      {
+        created_at: {
+          match: "created_at",
+          match_mapping_type: "date",
+          mapping: {
+            type: "date",
+            format: "dateOptionalTime",
+            fields: {
+              raw: {type: "date", format: "dateOptionalTime", index: "not_analyzed"}
+            }
+          }
+        }
+      },
+      {
+        ids: {
+          match: "^survey.*id$",
+          match_mapping_type: "long",
+          match_pattern: "regex",
+          mapping: {
+            type: "long"
+          }
+        }
       }
-    }
+    ]
+  }
+}
+
+
+ELASTIC_SEARCH_MAPPINGS = {
+  survey_responses: {
+    mappings: mappings
+  },
+  survey_responses_development: {
+    mappings: mappings
+  },
+  survey_responses_production: {
+    mappings: mappings
+  },
+  survey_responses_test: {
+    mappings: mappings
   }
 }
 
 unless ELASTIC_SEARCH_CLIENT.indices.exists index: ELASTIC_SEARCH_INDEX_NAME
-  ELASTIC_SEARCH_CLIENT.indices.create index: ELASTIC_SEARCH_INDEX_NAME, body: ELASTIC_SEARCH_MAPPINGS[:survey_responses]
+  ELASTIC_SEARCH_CLIENT.indices.create index: ELASTIC_SEARCH_INDEX_NAME, body: ELASTIC_SEARCH_MAPPINGS[ELASTIC_SEARCH_INDEX_NAME.to_sym]
 end
-
