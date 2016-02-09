98653de25f5a2755a39acc7f92bbadf108f5bb0d
Juan Alvarado <jalvarado@ctacorp.com>
2016-02-09 09:08:00 -0500

    Fix organizations search

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,7 @@
 class Organization < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+
+  scope :search, ->(q=nil) {
+    where("organizations.name LIKE ?", "%#{q}%") unless q.blank?
+  }
 end
