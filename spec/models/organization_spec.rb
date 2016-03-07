e4ae5220d10b7aea2a9568e65c0ebbde47060f05
Juan Alvarado <jalvarado@ctacorp.com>
2016-03-07 13:24:48 -0500

    Add length validatino for organizatin name

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Organization, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
+  it { should validate_length_of(:name).is_at_most(255) }
 end
