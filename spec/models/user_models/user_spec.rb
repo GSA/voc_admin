c6663dd6f5444802648434012bf8b35cb23ddee3
Juan Alvarado <jalvarado@ctacorp.com>
2016-07-25 15:22:24 -0400

    Remove hhs_id validation tests

--- spec/models/user_models/user_spec.rb
+++ spec/models/user_models/user_spec.rb
@@ -4,9 +4,6 @@ RSpec.describe User, type: :model do
   context "#validations" do
     it { should validate_presence_of(:f_name) }
     it { should validate_presence_of(:l_name) }
-    it { should validate_presence_of(:hhs_id) }
-    it { should validate_numericality_of(:hhs_id).only_integer }
-    it { should validate_length_of(:hhs_id).is_equal_to(10) }
   end
 
   context ".search" do
