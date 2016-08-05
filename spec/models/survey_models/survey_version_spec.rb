c157b54b6e9da6a531e13bc1453bb3f22a08244a
Juan Alvarado <jalvarado@ctacorp.com>
2016-08-05 16:29:16 -0400

    Test reporting count methods

--- spec/models/survey_models/survey_version_spec.rb
+++ spec/models/survey_models/survey_version_spec.rb
@@ -5,4 +5,116 @@ RSpec.describe SurveyVersion do
   it { should validate_presence_of(:minor) }
   it { should validate_numericality_of(:major) }
   it { should validate_presence_of(:survey) }
+
+  context "Reporting caches" do
+    it "increments the temp count for today by 1" do
+      survey_version = FactoryGirl.create(:survey_version)
+      expect(survey_version.temp_visit_count[date_string]).to eq nil
+
+      survey_version.increment_temp_visit_count
+
+      expect(survey_version.temp_visit_count[date_string]).to eq "1"
+    end
+
+    it "increments the temp count for invitations today by 1" do
+      survey_version = FactoryGirl.create(:survey_version)
+      expect(survey_version.temp_invitation_count[date_string]).to eq nil
+
+      survey_version.increment_temp_invitation_count
+
+      expect(survey_version.temp_invitation_count[date_string]).to eq "1"
+    end
+
+    it "increments the temp count for invitation_accepted by 1" do
+      survey_version = FactoryGirl.create(:survey_version)
+      expect(survey_version.temp_invitation_accepted_count[date_string]).to eq nil
+
+      survey_version.increment_temp_invitation_accepted_count
+
+      expect(survey_version.temp_invitation_accepted_count[date_string]).to eq "1"
+    end
+
+    context ".total_temp_visit_count" do
+      it "sums up the temp visit counts for the previous days" do
+        survey_version = FactoryGirl.create(:survey_version)
+        Timecop.freeze(1.day.ago) do
+          survey_version.increment_temp_visit_count
+        end
+        Timecop.freeze(Date.today) do
+          survey_version.increment_temp_visit_count
+        end
+        expect(survey_version.temp_visit_count.length).to eq 2
+        expect(survey_version.total_temp_visit_count).to eq 2
+      end
+    end
+
+    context ".total_temp_invitation_count" do
+      it "sums up the temp invitation counts for the previous days" do
+        survey_version = FactoryGirl.create(:survey_version)
+        Timecop.freeze(1.day.ago) do
+          survey_version.increment_temp_invitation_count
+        end
+        Timecop.freeze(Date.today) do
+          survey_version.increment_temp_invitation_count
+        end
+        expect(survey_version.total_temp_invitation_count).to eq 2
+      end
+    end
+
+    context ".total_temp_invitation_accepted_count" do
+      it "sums up the temp invitation accepted count" do
+        survey_version = FactoryGirl.create(:survey_version)
+        Timecop.freeze(1.day.ago) do
+          survey_version.increment_temp_invitation_accepted_count
+        end
+        Timecop.freeze(Date.today) do
+          survey_version.increment_temp_invitation_accepted_count
+        end
+        expect(survey_version.total_temp_invitation_accepted_count).to eq 2
+      end
+
+    end
+
+    describe ".total_invitation_accepted_count" do
+      context "when there are no survey_version_counts" do
+        it "returns the temp invitation count" do
+          survey_version = FactoryGirl.create(:survey_version)
+          survey_version.increment_temp_invitation_accepted_count
+          expect(survey_version.total_invitation_accepted_count).to eq 1
+        end
+      end
+
+      context "when there are previous survey_version_counts" do
+        it "returns the count from the survey_version_count object" do
+          survey_version = FactoryGirl.create(:survey_version)
+          Timecop.freeze(1.week.ago) do
+            survey_version.increment_temp_invitation_accepted_count
+          end
+          Timecop.freeze(1.week.ago + 1.day) do
+            survey_version.update_counts
+          end
+          survey_version.reload
+          expect(survey_version.survey_version_counts.count).to eq 1
+          expect(survey_version.total_invitation_accepted_count).to eq 1
+        end
+
+        it "adds the temp count for today to the stored counts" do
+          survey_version = FactoryGirl.create(:survey_version)
+          Timecop.freeze(1.week.ago) do
+            survey_version.increment_temp_invitation_accepted_count
+          end
+          Timecop.freeze(1.week.ago + 1.day) do
+            survey_version.update_counts
+          end
+          survey_version.increment_temp_invitation_accepted_count
+          survey_version.reload
+          expect(survey_version.total_invitation_accepted_count).to eq 2
+        end
+      end
+    end
+  end
+
+  def date_string
+    Date.today.strftime("%Y-%m-%d")
+  end
 end
