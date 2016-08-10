96660bad9c8700ec2fe2aa66e8174240733aa7d7
Juan Alvarado <jalvarado@ctacorp.com>
2016-08-10 09:14:27 -0400

    Use s6 to manage unicorn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :saml,
     issuer:             ENV.fetch('SAML_APP_NAME'),
-    idp_sso_target_url: ENV.fetch('SAML_CERT'),
-    idp_cert:           ENV.fetch('SSO_TARGET_URL')
+    idp_sso_target_url: ENV.fetch('SSO_TARGET_URL'),
+    idp_cert:           ENV.fetch('SAML_CERT')
 end
