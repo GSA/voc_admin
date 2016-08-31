e2f022fde79f2dd7a6bae5c34e1b7f0341e7f48e
Jake Vose <jake.vose@gmail.com>
2016-08-31 10:15:42 -0400

    fixing bundle command

--- bundle.sh
+++ bundle.sh
@@ -2,7 +2,7 @@ N=0
 STATUS=1
 until [ ${N} -ge 5 ]
 do
-  bundle install --without development test --jobs 4 --deployment && STATUS=0 && break
+  bundle install --without development test --jobs 4 && STATUS=0 && break
   echo 'Try bundle again ...'
   N=$[${N}+1]
   sleep 1
