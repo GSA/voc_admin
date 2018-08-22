6006b461c3ad785fe1c0b5f678f8ceab7a0a599c
Jake Vose <jake.vose@gmail.com>
2018-08-22 08:49:57 -0400

    restoring operation of the local dev environment using docker-compose

--- development.sh
+++ development.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./wait-for-it.sh -t 0 mysql:3306 -- bundle exec rake db:create db:migrate && bundle exec rake db:seed && bundle exec rails s --port 80 --binding '0.0.0.0'
+./wait-for-it.sh -t 0 elasticsearch:9200 -- bundle exec rake db:create db:migrate && bundle exec rake db:seed && bundle exec rails s --port 80 --binding '0.0.0.0'
